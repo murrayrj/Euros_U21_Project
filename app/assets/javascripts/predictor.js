d34da4ac236609ce42985a9dec093311233855fc
Richard Murray-Clark <richardmurray-clark@hotmail.com>
2015-06-11 19:49:55 +0100

    Added some styling

--- app/assets/javascripts/predictor.js
+++ app/assets/javascripts/predictor.js
@@ -48,17 +48,43 @@ $(document).ready(function() {
       console.log(data, response)
     });
   });
+  $("#groupB").on("click", function() {
+    var $matches = $('.match');
+    var $results = $.map($matches, function(match, index){
+      var $homeTeamName  = $(match).find('.home').text();
+      var $awayTeamName  = $(match).find('.away').text();
+      var $homeTeamScore = $(match).find('select#home').val();
+      var $awayTeamScore = $(match).find('select#away').val();
+      
+      console.log($homeTeamName, $awayTeamName, $homeTeamScore, $awayTeamScore)
+
+      return [{
+        name: $homeTeamName,
+        score: $homeTeamScore,
+        result: result($homeTeamScore, $awayTeamScore)
+      },
+      {
+        name: $awayTeamName,
+        score: $awayTeamScore,
+        result: result($awayTeamScore, $homeTeamScore)
+      }]
+    })
+
+    console.log($results);
+
+    $.ajax({
+      beforeSend: function(xhr) {
+        xhr.setRequestHeader(
+            'X-CSRF-Token', 
+            $('meta[name="csrf-token"]').attr('content'))},
+      url: "/teams/update_teams",
+      type: "POST",
+      contentType: "application/json",
+      data: JSON.stringify({
+        teams: $results      
+      })
+    }).done(function(data, response) {
+      console.log(data, response)
+    });
+  });
 });
-
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   console.log('HELLO WORLD');
-
-//   getRegionsButton = $('#get-regions');
-//   regionSelect = $('#region-select');
-//   countrySelect = $('#country-select');
-//   results = $('#results');
-
-//   getRegionsButton.on('click', getRegions);
-//   regionSelect.on('change', getCountries);
-//   countrySelect.on('change', getCountryData);
--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -3,14 +3,6 @@ class GroupController < ApplicationController
     @groups = Group.all
     @groupA = Group.all.select{|group| group.name == "Group A"}
     @groupB = Group.all.select{|group| group.name == "Group B"}
-    @CZE = Team.all.select{|team| team.code == 'CZE'}.first
-    @DEN = Team.all.select{|team| team.code == 'DEN'}.first
-    @GER = Team.all.select{|team| team.code == 'GER'}.first
-    @SRB = Team.all.select{|team| team.code == 'SRB'}.first
-    @ENG = Team.all.select{|team| team.code == 'ENG'}.first
-    @ITA = Team.all.select{|team| team.code == 'ITA'}.first
-    @POR = Team.all.select{|team| team.code == 'POR'}.first
-    @SWE = Team.all.select{|team| team.code == 'SWE'}.first
   end
 
   def show
